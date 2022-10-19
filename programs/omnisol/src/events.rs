22d924ef7254162049f0797c776ef2a19304bd2a
tiamo <vk.tiamo@gmail.com>
2022-10-19 14:19:46 +0300

    feat(program): added events

--- programs/omnisol/src/events.rs
+++ programs/omnisol/src/events.rs
@@ -1,3 +1,21 @@
+use crate::*;
+
 #[event]
-pub struct SomeEvent {
+pub struct DepositStakeEvent {
+    #[index]
+    pub pool: Pubkey,
+    #[index]
+    pub stake: Pubkey,
+    pub amount: u64,
+    pub timestamp: i64,
+}
+
+#[event]
+pub struct WithdrawEvent {
+    #[index]
+    pub pool: Pubkey,
+    #[index]
+    pub stake: Pubkey,
+    pub amount: u64,
+    pub timestamp: i64,
 }
