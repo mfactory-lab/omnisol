5228016b1f867d8c359ca8a279ab6f0ec18ef1d9
tiamo <vk.tiamo@gmail.com>
2022-10-21 03:23:04 +0300

    feat: added `Withdrawal` state

--- programs/omnisol/src/state.rs
+++ programs/omnisol/src/state.rs
@@ -6,14 +6,14 @@ pub struct Pool {
     pub pool_mint: Pubkey,
     /// An account with authority that can manage and close the pool.
     pub authority: Pubkey,
+    pub deposit_count: u64,
+    pub withdraw_count: u64,
     /// Signer bump seed for deriving PDA seeds
     pub authority_bump: u8,
-    /// Bump seed for deriving PDA seeds.
-    pub bump: u8,
 }
 
 impl Pool {
-    pub const SIZE: usize = 66; // 32 + 32 + 1 + 1;
+    pub const SIZE: usize = 32 + 32 + 8 + 8 + 1;
 }
 
 #[account]
@@ -24,13 +24,24 @@ pub struct Pledge {
     pub split_stake: Pubkey,
     pub stake_delegation: u64,
     pub amount: u64,
-    // pub last_withdraw_amount: u64,
-    // pub last_withdraw_at: i64,
     pub created_at: i64,
-    /// Bump seed for deriving PDA seeds.
     pub bump: u8,
 }
 
 impl Pledge {
+    pub const SEED: &'static [u8] = b"pledge";
     pub const SIZE: usize = 32 + 32 + 32 + 32 + 8 + 8 + 8 + 1;
+}
+
+#[account]
+pub struct Withdrawal {
+    pub id: u64,
+    pub authority: Pubkey,
+    pub amount: u64,
+    pub bump: u8,
+}
+
+impl Withdrawal {
+    pub const SEED: &'static [u8] = b"withdrawal";
+    pub const SIZE: usize = 8 + 32 + 8 + 1;
 }
