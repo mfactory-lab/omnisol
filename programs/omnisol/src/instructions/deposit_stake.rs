7b0cd717335d9770ab175ad57fe239a08c0f63b8
tiamo <vk.tiamo@gmail.com>
2022-10-22 03:11:16 +0300

    fix: `deposit_stake` improved

--- programs/omnisol/src/instructions/deposit_stake.rs
+++ programs/omnisol/src/instructions/deposit_stake.rs
@@ -1,10 +1,11 @@
 use anchor_lang::{prelude::*, solana_program::stake::state::StakeAuthorize};
-use anchor_spl::token::{self, Token, TokenAccount};
+use anchor_spl::token;
 
 use crate::{
     events::*,
-    state::{Pledge, Pool},
-    utils::stake::{self, Stake, StakeAccount},
+    state::{Collateral, Pool},
+    utils::stake,
+    ErrorCode,
 };
 
 /// The user can use their deposit as collateral and mint omniSOL.
@@ -15,36 +16,46 @@ use crate::{
 pub fn handle(ctx: Context<DepositStake>, amount: u64) -> Result<()> {
     let pool = &mut ctx.accounts.pool;
 
-    // Split new stake from existing stake
-    stake::split(
-        CpiContext::new_with_signer(
-            ctx.accounts.stake_program.to_account_info(),
-            stake::Split {
-                stake: ctx.accounts.source_stake.to_account_info(),
-                split_stake: ctx.accounts.split_stake.to_account_info(),
-                authority: ctx.accounts.stake_authority.to_account_info(),
-                system_program: ctx.accounts.system_program.to_account_info(),
-            },
-            &[],
-        ),
-        amount,
-    )?;
+    let delegation = ctx
+        .accounts
+        .source_stake
+        .delegation()
+        .ok_or(ErrorCode::InvalidStakeAccount)?;
 
     let pool_key = pool.key();
     let pool_authority_seeds = [pool_key.as_ref(), &[pool.authority_bump]];
     let clock = &ctx.accounts.clock;
 
-    // Authorize to withdraw the split stake for the program
+    let stake_account = if amount < delegation.stake {
+        // Split new stake from existing stake
+        stake::split(
+            CpiContext::new_with_signer(
+                ctx.accounts.stake_program.to_account_info(),
+                stake::Split {
+                    stake: ctx.accounts.source_stake.to_account_info(),
+                    split_stake: ctx.accounts.split_stake.to_account_info(),
+                    authority: ctx.accounts.authority.to_account_info(),
+                    system_program: ctx.accounts.system_program.to_account_info(),
+                },
+                &[],
+            ),
+            amount,
+        )?;
+        ctx.accounts.split_stake.to_account_info()
+    } else {
+        ctx.accounts.source_stake.to_account_info()
+    };
+
+    // Authorize to `withdraw` the split stake for the program
     stake::authorize(
-        CpiContext::new_with_signer(
+        CpiContext::new(
             ctx.accounts.stake_program.to_account_info(),
             stake::Authorize {
-                stake: ctx.accounts.split_stake.to_account_info(),
-                authority: ctx.accounts.stake_authority.to_account_info(),
+                stake: stake_account,
+                authority: ctx.accounts.authority.to_account_info(),
                 new_authority: ctx.accounts.pool_authority.to_account_info(),
                 clock: clock.to_account_info(),
             },
-            &[&pool_authority_seeds],
         ),
         StakeAuthorize::Withdrawer,
         None,
@@ -64,27 +75,25 @@ pub fn handle(ctx: Context<DepositStake>, amount: u64) -> Result<()> {
         amount,
     )?;
 
-    let stake = &ctx.accounts.source_stake;
-    let stake_delegation = stake.stake().unwrap().delegation.stake;
+    let collateral = &mut ctx.accounts.collateral;
 
-    let pledge = &mut ctx.accounts.pledge;
+    collateral.authority = ctx.accounts.authority.key();
+    collateral.pool = pool_key;
+    collateral.source_stake = ctx.accounts.source_stake.key();
+    collateral.split_stake = stake_account.key();
+    collateral.delegation_stake = delegation.stake;
+    collateral.amount = amount;
+    collateral.created_at = clock.unix_timestamp;
+    collateral.bump = ctx.bumps["collateral"];
 
-    pledge.authority = ctx.accounts.authority.key();
-    pledge.pool = pool_key;
-    pledge.source_stake = stake.key();
-    pledge.split_stake = ctx.accounts.split_stake.key();
-    pledge.stake_delegation = stake_delegation;
-    pledge.amount = amount;
-    pledge.created_at = clock.unix_timestamp;
-    pledge.bump = ctx.bumps["pledge"];
-
-    pool.deposit_count = pool.deposit_count.saturating_add(1);
+    pool.deposit_amount = pool.deposit_amount.saturating_add(amount);
 
     emit!(DepositStakeEvent {
         pool: pool.key(),
-        pledge: pledge.key(),
-        amount,
+        collateral: collateral.key(),
+        delegation_stake: collateral.delegation_stake,
         timestamp: clock.unix_timestamp,
+        amount,
     });
 
     Ok(())
@@ -95,8 +104,8 @@ pub struct DepositStake<'info> {
     #[account(mut)]
     pub pool: Box<Account<'info, Pool>>,
 
-    #[account(mut)]
-    pub pool_mint: Box<Account<'info, token::Mint>>,
+    #[account(address = pool.pool_mint)]
+    pub pool_mint: AccountInfo<'info>,
 
     /// CHECK: no needs to check, only for signing
     #[account(seeds = [pool.key().as_ref()], bump = pool.authority_bump)]
@@ -104,29 +113,31 @@ pub struct DepositStake<'info> {
 
     #[account(
         init,
-        seeds = [Pledge::SEED, pool.key().as_ref(), source_stake.key().as_ref()],
+        seeds = [Collateral::SEED, pool.key().as_ref(), source_stake.key().as_ref()],
         bump,
         payer = authority,
-        space = Pledge::SIZE,
+        space = Collateral::SIZE,
     )]
-    pub pledge: Box<Account<'info, Pledge>>,
+    pub collateral: Box<Account<'info, Collateral>>,
+
+    #[account(
+        mut,
+        associated_token::mint = pool_mint,
+        associated_token::authority = authority,
+    )]
+    pub user_pool_token: Account<'info, token::TokenAccount>,
 
     #[account(mut)]
-    pub user_pool_token: Account<'info, TokenAccount>,
-
-    #[account(mut)]
-    pub source_stake: Account<'info, StakeAccount>,
+    pub source_stake: Account<'info, stake::StakeAccount>,
     /// CHECK:
-    #[account(mut)]
+    #[account(mut, signer)]
     pub split_stake: AccountInfo<'info>,
-    /// CHECK:
-    pub stake_authority: Signer<'info>,
 
     #[account(mut)]
     pub authority: Signer<'info>,
 
     pub clock: Sysvar<'info, Clock>,
-    pub stake_program: Program<'info, Stake>,
-    pub token_program: Program<'info, Token>,
+    pub stake_program: Program<'info, stake::Stake>,
+    pub token_program: Program<'info, token::Token>,
     pub system_program: Program<'info, System>,
 }
