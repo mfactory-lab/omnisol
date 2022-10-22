57b80ef85fa9bbd0b871f352193bbec8a75afe6c
tiamo <vk.tiamo@gmail.com>
2022-10-22 03:09:52 +0300

    fix: state refactored

--- programs/omnisol/src/state.rs
+++ programs/omnisol/src/state.rs
@@ -6,42 +6,29 @@ pub struct Pool {
     pub pool_mint: Pubkey,
     /// An account with authority that can manage and close the pool.
     pub authority: Pubkey,
-    pub deposit_count: u64,
-    pub withdraw_count: u64,
+    /// Total stake in deposit
+    pub deposit_amount: u64,
     /// Signer bump seed for deriving PDA seeds
     pub authority_bump: u8,
 }
 
 impl Pool {
-    pub const SIZE: usize = 32 + 32 + 8 + 8 + 1;
+    pub const SIZE: usize = 32 + 32 + 8 + 1;
 }
 
 #[account]
-pub struct Pledge {
+pub struct Collateral {
     pub authority: Pubkey,
     pub pool: Pubkey,
     pub source_stake: Pubkey,
     pub split_stake: Pubkey,
-    pub stake_delegation: u64,
+    pub delegation_stake: u64,
     pub amount: u64,
     pub created_at: i64,
     pub bump: u8,
 }
 
-impl Pledge {
-    pub const SEED: &'static [u8] = b"pledge";
+impl Collateral {
+    pub const SEED: &'static [u8] = b"collateral";
     pub const SIZE: usize = 32 + 32 + 32 + 32 + 8 + 8 + 8 + 1;
-}
-
-#[account]
-pub struct Withdrawal {
-    pub id: u64,
-    pub authority: Pubkey,
-    pub amount: u64,
-    pub bump: u8,
-}
-
-impl Withdrawal {
-    pub const SEED: &'static [u8] = b"withdrawal";
-    pub const SIZE: usize = 8 + 32 + 8 + 1;
 }
