e048a58852861770128cb7da65d364d439f5f5f8
tiamo <vk.tiamo@gmail.com>
2022-10-22 03:12:01 +0300

    fix: `withdraw_sol` improved

--- programs/omnisol/src/instructions/withdraw_sol.rs
+++ programs/omnisol/src/instructions/withdraw_sol.rs
@@ -1,12 +1,10 @@
 use anchor_lang::prelude::*;
-use anchor_spl::{
-    token,
-    token::{Token, TokenAccount},
-};
+use anchor_spl::token::{self, Token, TokenAccount};
 
 use crate::{
     events::*,
-    state::{Pool, Withdrawal},
+    state::Pool,
+    utils::{stake, unstake_it},
 };
 
 /// Withdraw a given amount of omniSOL (without an account).
@@ -15,16 +13,11 @@ use crate::{
 /// TODO: get account from priority queue
 /// TODO: liquidate the account
 /// TODO: transfer sol or split stake?
-pub fn handle(ctx: Context<WithdrawSol>, amount: u64) -> Result<()> {
+pub fn handle<'info>(ctx: Context<'_, '_, '_, 'info, WithdrawSol<'info>>, amount: u64) -> Result<()> {
     let pool = &mut ctx.accounts.pool;
-    let withdrawal = &mut ctx.accounts.withdrawal;
 
-    withdrawal.id = pool.withdraw_count;
-    withdrawal.authority = ctx.accounts.authority.key();
-    withdrawal.amount = amount;
-    withdrawal.bump = ctx.bumps["withdrawal"];
-
-    pool.withdraw_count = pool.withdraw_count.saturating_add(1);
+    let pool_key = pool.key();
+    let pool_authority_seeds = [pool_key.as_ref(), &[pool.authority_bump]];
 
     token::burn(
         CpiContext::new(
@@ -38,15 +31,43 @@ pub fn handle(ctx: Context<WithdrawSol>, amount: u64) -> Result<()> {
         amount,
     )?;
 
-    let timestamp = Clock::get()?.unix_timestamp;
+    // TODO: get from oracle
+    let stake_account = ctx.remaining_accounts.get(0).unwrap();
 
-    emit!(WithdrawSolEvent {
-        pool: pool.key(),
-        withdrawal: withdrawal.key(),
-        authority: withdrawal.authority.key(),
-        amount,
-        timestamp,
-    });
+    let pool_account = ctx.remaining_accounts.get(0).expect("Expect #0 account");
+    let pool_sol_reserves = ctx.remaining_accounts.get(1).expect("Expect #1 account");
+    let fee_account = ctx.remaining_accounts.get(2).expect("Expect #2 account");
+    let stake_account_record_account = ctx.remaining_accounts.get(3).expect("Expect #3 account");
+    let protocol_fee_account = ctx.remaining_accounts.get(4).expect("Expect #4 account");
+    let protocol_fee_destination = ctx.remaining_accounts.get(5).expect("Expect #5 account");
+
+    unstake_it::unstake(CpiContext::new_with_signer(
+        ctx.accounts.token_program.to_account_info(),
+        unstake_it::Unstake {
+            payer: ctx.accounts.authority.to_account_info(),
+            unstaker: ctx.accounts.pool_authority.to_account_info(),
+            stake_account: stake_account.to_account_info(),
+            destination: ctx.accounts.authority.to_account_info(), // TODO: destination account
+            pool_account: pool_account.to_account_info(),
+            pool_sol_reserves: pool_sol_reserves.to_account_info(),
+            fee_account: fee_account.to_account_info(),
+            stake_account_record_account: stake_account_record_account.to_account_info(),
+            protocol_fee_account: protocol_fee_account.to_account_info(),
+            protocol_fee_destination: protocol_fee_destination.to_account_info(),
+            clock: ctx.accounts.clock.to_account_info(),
+            stake_program: ctx.accounts.stake_program.to_account_info(),
+            system_program: ctx.accounts.system_program.to_account_info(),
+        },
+        &[&pool_authority_seeds],
+    ))?;
+
+    // let timestamp = Clock::get()?.unix_timestamp;
+    // emit!(WithdrawSolEvent {
+    //     pool: pool.key(),
+    //     authority: withdrawal.owner.key(),
+    //     amount,
+    //     timestamp,
+    // });
 
     Ok(())
 }
@@ -56,17 +77,13 @@ pub struct WithdrawSol<'info> {
     #[account(mut)]
     pub pool: Box<Account<'info, Pool>>,
 
+    /// CHECK: token program will check it
     #[account(address = pool.pool_mint)]
-    pub pool_mint: Account<'info, token::Mint>,
+    pub pool_mint: AccountInfo<'info>,
 
-    #[account(
-        init,
-        seeds = [Withdrawal::SEED, pool.key().as_ref(), pool.withdraw_count.to_le_bytes()],
-        bump,
-        payer = authority,
-        space = Withdrawal::SIZE,
-    )]
-    pub withdrawal: Box<Account<'info, Withdrawal>>,
+    /// CHECK: no needs to check, only for signing
+    #[account(seeds = [pool.key().as_ref()], bump = pool.authority_bump)]
+    pub pool_authority: AccountInfo<'info>,
 
     #[account(
         mut,
@@ -78,6 +95,8 @@ pub struct WithdrawSol<'info> {
     #[account(mut)]
     pub authority: Signer<'info>,
 
+    pub clock: Sysvar<'info, Clock>,
+    pub stake_program: Program<'info, stake::Stake>,
     pub token_program: Program<'info, Token>,
     pub system_program: Program<'info, System>,
 }
