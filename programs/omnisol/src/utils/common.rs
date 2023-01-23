64c6b089ce3c61422cbd0b399f2e8ef9110df40b
Illia Fedotov <illia.fedotov@litslink.com>
2023-01-23 23:58:45 +0200

    fix: fix PDA closing

--- programs/omnisol/src/utils/common.rs
+++ programs/omnisol/src/utils/common.rs
@@ -1,7 +1,7 @@
-use anchor_lang::{
-    solana_program::{account_info::AccountInfo, system_program},
-    Result,
-};
+use std::io::{Cursor, Write};
+use std::ops::DerefMut;
+use anchor_lang::{solana_program::{account_info::AccountInfo, system_program}, Result, error, error::ErrorCode};
+use anchor_lang::__private::CLOSED_ACCOUNT_DISCRIMINATOR;
 
 pub fn close<'info>(info: AccountInfo<'info>, sol_destination: AccountInfo<'info>) -> Result<()> {
     // Transfer lamports from the account to the sol_destination.
@@ -9,6 +9,15 @@ pub fn close<'info>(info: AccountInfo<'info>, sol_destination: AccountInfo<'info
     **sol_destination.lamports.borrow_mut() = dest_starting_lamports.checked_add(info.lamports()).unwrap();
     **info.lamports.borrow_mut() = 0;
 
-    info.assign(&system_program::ID);
-    info.realloc(0, false).map_err(Into::into)
+    // Clean account data
+    let mut data = info.try_borrow_mut_data()?;
+    for byte in data.deref_mut().iter_mut() {
+        *byte = 0;
+    }
+
+    // Mark the account discriminator as closed.
+    let dst: &mut [u8] = &mut data;
+    let mut cursor = Cursor::new(dst);
+    cursor.write_all(&CLOSED_ACCOUNT_DISCRIMINATOR)
+        .map_err(|_| error!(ErrorCode::AccountDidNotSerialize))
 }
